efb32370347ac5075aa7e0c6a3705339ed0ecb2c
Mohammed <mohammedmiessal@gmail.com>
2024-02-27 12:21:50 +0100

    TAS-2 Task management is successfully finished

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\TaskController;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
 
@@ -10,6 +11,7 @@ use Illuminate\Http\Request;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register API routes for your application. These
+
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
 |
@@ -24,4 +26,17 @@ Route::group([], function () {
     Route::post('register', [AuthController::class, 'register'])->name('register');
     Route::post('logout', [AuthController::class, 'logout'])->name('logout');
     Route::post('refresh', [AuthController::class, 'refresh'])->name('refresh');
+});
+
+
+Route::prefix('task')->group(function () {
+    Route::get('/', [TaskController::class, 'index'])->name('task.read');
+    Route::get('/index', [TaskController::class, 'index'])->name('task.read');
+    Route::get('/create', [TaskController::class, 'create'])->name('task.create');
+    Route::post('/store', [TaskController::class, 'store'])->name('task.store');
+    Route::get('/show/{id}', [TaskController::class, 'show'])->name('task.show');
+    Route::get('/edit/{task}', [TaskController::class, 'edit'])->name('task.edit');
+    Route::put('/update/{task}', [TaskController::class, 'update'])->name('task.update');
+    Route::delete('/destroy/{task}', [TaskController::class, 'destroy'])->name('task.destroy');
+    Route::get('/showbyuser', [TaskController::class, 'showTaskByUser'])->name('task.showbyuser');
 });
